d32781292423fe2c5fbaf12e6f734114499b12ae
jbmourad <mouradjebouai@gmail.com>
2022-04-08 12:38:53 +0100

    Comments

--- src/main/java/tn/mesrs/Test.java
+++ src/main/java/tn/mesrs/Test.java
@@ -1,7 +1,12 @@
+/*
+ Commentaires
+ */
+
 package tn.mesrs;
 public class Test{
 
     public static void main(String[] args){
+        //********** */
         System.out.println("Hello!");
     }
 }
